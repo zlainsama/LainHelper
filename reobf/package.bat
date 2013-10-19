b0a3734140f82754be41530acb84ad9ed4da559a
玲音 <zlainsama@163.com>
2013-10-19 16:44:59 +0800

    backup

--- reobf/package.bat
+++ reobf/package.bat
@@ -1,2 +1,2 @@
 del *.jar
-jar cvf "[1.6.x-v4] LainHelper.jar" -C minecraft lain/mods/helper/ -C resources/helper/ .
+jar cvf "[1.6.x-v5] LainHelper.jar" -C minecraft lain/mods/helper/ -C resources/helper/ .
--- src/minecraft/lain/mods/helper/LainHelper.java
+++ src/minecraft/lain/mods/helper/LainHelper.java
@@ -1,6 +1,5 @@
 package lain.mods.helper;
 
-import java.util.EnumSet;
 import lain.mods.helper.tile.BlockCobbleCube;
 import lain.mods.helper.tile.BlockWaterCube;
 import lain.mods.helper.tile.TileCobbleCube;
@@ -11,24 +10,19 @@ import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.potion.Potion;
 import net.minecraftforge.common.Configuration;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.ForgeSubscribe;
 import net.minecraftforge.event.entity.living.LivingAttackEvent;
 import net.minecraftforge.event.entity.living.LivingHurtEvent;
-import cpw.mods.fml.common.ITickHandler;
 import cpw.mods.fml.common.Mod;
-import cpw.mods.fml.common.TickType;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.registry.GameRegistry;
 import cpw.mods.fml.common.registry.LanguageRegistry;
-import cpw.mods.fml.common.registry.TickRegistry;
-import cpw.mods.fml.relauncher.Side;
 
-@Mod(modid = "LainHelper", name = "LainHelper", version = "1.6.x-v4")
-public class LainHelper implements ITickHandler
+@Mod(modid = "LainHelper", name = "LainHelper", version = "1.6.x-v5")
+public class LainHelper
 {
 
     public static Block blockWaterCube;
@@ -41,12 +35,6 @@ public class LainHelper implements ITickHandler
         if (obj instanceof EntityPlayer)
             return checkOwner(((EntityPlayer) obj).username);
         return false;
-    }
-
-    @Override
-    public String getLabel()
-    {
-        return "LainHelper";
     }
 
     @Mod.EventHandler
@@ -86,7 +74,6 @@ public class LainHelper implements ITickHandler
     public void load(FMLInitializationEvent event)
     {
         MinecraftForge.EVENT_BUS.register(this);
-        TickRegistry.registerTickHandler(this, Side.SERVER);
 
         GameRegistry.registerBlock(blockWaterCube, "blockWaterCube");
         GameRegistry.registerTileEntity(TileWaterCube.class, "tileWaterCube");
@@ -120,34 +107,6 @@ public class LainHelper implements ITickHandler
                 event.setCanceled(true);
             else if (event.source.isMagicDamage())
                 event.setCanceled(true);
-        }
-    }
-
-    @Override
-    public void tickEnd(EnumSet<TickType> type, Object... tickData)
-    {
-    }
-
-    @Override
-    public EnumSet<TickType> ticks()
-    {
-        return EnumSet.of(TickType.PLAYER);
-    }
-
-    @Override
-    public void tickStart(EnumSet<TickType> type, Object... tickData)
-    {
-        if (type.contains(TickType.PLAYER))
-        {
-            EntityPlayer plr = (EntityPlayer) tickData[0];
-            if (checkOwner(plr))
-                if (plr.isEntityAlive())
-                {
-                    if (plr.isPotionActive(Potion.hunger.getId()))
-                        plr.removePotionEffect(Potion.hunger.getId());
-                    if (plr.isPotionActive(Potion.poison.getId()))
-                        plr.removePotionEffect(Potion.poison.getId());
-                }
         }
     }
 
