ee88ac0d8f4e375ad8b9dac5650f30e0647dacb6
玲音 <zlainsama@163.com>
2013-12-13 01:41:38 +0800

    backup

--- src/main/java/lain/mods/helper/Vampirism.java
+++ src/main/java/lain/mods/helper/Vampirism.java
@@ -46,19 +46,16 @@ public class Vampirism implements ITickHandler
             {
                 if (player.worldObj.getTotalWorldTime() % 40L == 0L)
                 {
-                    boolean flag = player.worldObj.isDaytime() && player.getBrightness(1.0F) > 0.5F && player.worldObj.canBlockSeeTheSky(MathHelper.floor_double(player.posX), MathHelper.floor_double(player.posY + player.getEyeHeight()), MathHelper.floor_double(player.posZ));
+                    boolean flag = LainHelper.sunlightDebuff && player.worldObj.isDaytime() && player.getBrightness(1.0F) > 0.5F && player.worldObj.canBlockSeeTheSky(MathHelper.floor_double(player.posX), MathHelper.floor_double(player.posY + player.getEyeHeight()), MathHelper.floor_double(player.posZ));
 
                     if (flag)
                     {
-                        if (LainHelper.sunlightDebuff)
+                        player.addPotionEffect(new PotionEffect(Potion.blindness.id, 200, 0, true));
+                        if (LainHelper.vampirePower)
                         {
-                            player.addPotionEffect(new PotionEffect(Potion.blindness.id, 200, 0, true));
-                            if (LainHelper.vampirePower)
-                            {
-                                player.addPotionEffect(new PotionEffect(Potion.digSlowdown.id, 200, 2, true));
-                                player.addPotionEffect(new PotionEffect(Potion.moveSlowdown.id, 200, 2, true));
-                                player.addPotionEffect(new PotionEffect(Potion.weakness.id, 200, 2, true));
-                            }
+                            player.addPotionEffect(new PotionEffect(Potion.digSlowdown.id, 200, 2, true));
+                            player.addPotionEffect(new PotionEffect(Potion.moveSlowdown.id, 200, 2, true));
+                            player.addPotionEffect(new PotionEffect(Potion.weakness.id, 200, 2, true));
                         }
                     }
                     else
