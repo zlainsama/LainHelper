5ac0fbd37acbcc65ea30b3efdfc8b019707e767c
玲音 <zlainsama@163.com>
2014-04-24 11:57:50 +0800

    backup

--- src/main/java/lain/mods/helper/note/NOTE.java
+++ src/main/java/lain/mods/helper/note/NOTE.java
@@ -61,12 +61,12 @@ public final class NOTE
             if (ai != null && ai.getModifier(_SAID) == null)
             {
                 float mh = player.getMaxHealth();
-                ai.applyModifier(new AttributeModifier(_SAID, _ID, 0.2D, 2));
+                ai.applyModifier(new AttributeModifier(_SAID, _ID, 1.0D, 2));
                 player.heal(player.getMaxHealth() - mh);
             }
             ai = player.getEntityAttribute(SharedMonsterAttributes.attackDamage);
             if (ai != null && ai.getModifier(_SAID) == null)
-                ai.applyModifier(new AttributeModifier(_SAID, _ID, 1.5D, 1));
+                ai.applyModifier(new AttributeModifier(_SAID, _ID, 1.0D, 2));
         }
     }
 
