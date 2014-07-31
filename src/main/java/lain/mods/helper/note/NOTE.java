c216bf973abd08b167c86b197e7b99413388ff59
玲音 <zlainsama@163.com>
2014-07-31 10:22:02 +0800

    backup

--- src/main/java/lain/mods/helper/note/NOTE.java
+++ src/main/java/lain/mods/helper/note/NOTE.java
@@ -51,7 +51,7 @@ public final class NOTE
         {
             IAttributeInstance ai = player.getEntityAttribute(ModAttributes.damageReduction);
             if (ai instanceof ModifiableAttributeInstance && ai.getModifier(_SAID) == null)
-                ai.applyModifier(new AttributeModifier(_SAID, _ID, 1.0D, 0));
+                ai.applyModifier(new AttributeModifier(_SAID, _ID, 0.4D, 0));
         }
     }
 
