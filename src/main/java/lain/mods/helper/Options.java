0a1bc9e94cb0d57c5e42a54c2a28dbae2f92f9d8
玲音 <zlainsama@163.com>
2014-08-12 14:47:58 +0800

    backup

--- src/main/java/lain/mods/helper/Options.java
+++ src/main/java/lain/mods/helper/Options.java
@@ -12,7 +12,7 @@ public class Options
     public static boolean enableHelperCommands = false;
     public static boolean overworldHomeOnly = true;
     public static boolean enableSharedStorage = false;
-    public static boolean enableSurvivalists = true;
+    public static boolean enableSurvivalists = false;
 
     @SuppressWarnings({ "unchecked", "rawtypes" })
     public static void loadConfig(Configuration config, Logger logger)
