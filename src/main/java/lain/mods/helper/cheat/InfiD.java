d610d269661d69c61c8f9fde93da7da0ac273c94
玲音 <zlainsama@163.com>
2014-08-27 12:16:08 +0800

    backup

--- src/main/java/lain/mods/helper/cheat/InfiD.java
+++ src/main/java/lain/mods/helper/cheat/InfiD.java
@@ -1,20 +1,39 @@
 package lain.mods.helper.cheat;
 
+import java.util.concurrent.atomic.AtomicBoolean;
 import lain.mods.helper.note.Note;
-import net.minecraft.entity.Entity;
+import lain.mods.helper.note.NoteClient;
+import net.minecraft.client.entity.EntityClientPlayerMP;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
-import net.minecraft.util.DamageSource;
+import net.minecraft.util.FoodStats;
+import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.entity.living.LivingHurtEvent;
+import cpw.mods.fml.common.FMLCommonHandler;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import cpw.mods.fml.common.gameevent.TickEvent;
 
 public class InfiD
 {
 
     public static void load()
     {
-        MinecraftForge.EVENT_BUS.register(new InfiD());
+        InfiD iD = !FMLCommonHandler.instance().getSide().isClient() ? new InfiD() : new InfiD()
+        {
+            @Override
+            protected void tickPlayer(EntityPlayer player)
+            {
+                if (player instanceof EntityClientPlayerMP)
+                    processClient(player);
+                super.tickPlayer(player);
+            }
+        };
+        MinecraftForge.EVENT_BUS.register(iD);
+        FMLCommonHandler.instance().bus().register(iD);
     }
+
+    private AtomicBoolean skipRender = new AtomicBoolean(false);
 
     private InfiD()
     {
@@ -23,19 +42,89 @@ public class InfiD
     @SubscribeEvent
     public void handleEvent(LivingHurtEvent event)
     {
-        Entity attacker = event.source.getEntity();
         if (event.entityLiving instanceof EntityPlayerMP)
         {
             if (Note.getNote((EntityPlayerMP) event.entityLiving).get("InfiD") != null)
             {
                 if (event.ammount > 0)
-                {
-                    if (attacker != null && event.entityLiving != attacker)
-                        attacker.attackEntityFrom(DamageSource.causeThornsDamage(event.entity), 4.0F);
-                    event.ammount *= 0.2F;
-                }
+                    event.ammount *= 0.5F;
             }
         }
+    }
+
+    @SubscribeEvent
+    public void handleEvent(RenderGameOverlayEvent.Pre event)
+    {
+        switch (event.type)
+        {
+            case FOOD:
+                if (skipRender.get())
+                    event.setCanceled(true);
+                break;
+            case AIR:
+                if (skipRender.get())
+                    event.setCanceled(true);
+                break;
+            default:
+                break;
+        }
+    }
+
+    @SubscribeEvent
+    public void handleEvent(TickEvent.PlayerTickEvent event)
+    {
+        if (event.phase == TickEvent.Phase.END)
+            tickPlayer(event.player);
+    }
+
+    protected void processClient(EntityPlayer player)
+    {
+        if (NoteClient.instance().get("InfiD") != null)
+        {
+            FoodStats food = player.getFoodStats();
+            if (food != null)
+            {
+                food.addStats(-food.getFoodLevel(), 0.0F);
+                food.addStats(10, 20.0F);
+                food.addStats(8, 0.0F);
+            }
+
+            if (player.getAir() < 100)
+                player.setAir(player.getAir() + 200);
+
+            player.extinguish();
+
+            skipRender.compareAndSet(false, true);
+        }
+        else
+        {
+            skipRender.compareAndSet(true, false);
+        }
+    }
+
+    protected void processServer(EntityPlayer player)
+    {
+        if (Note.getNote((EntityPlayerMP) player).get("InfiD") != null)
+        {
+            FoodStats food = player.getFoodStats();
+            if (food != null)
+            {
+                food.addStats(-food.getFoodLevel(), 0.0F);
+                food.addStats(10, 20.0F);
+                food.addStats(8, 0.0F);
+            }
+
+            if (player.getAir() < 100)
+                player.setAir(player.getAir() + 200);
+
+            player.extinguish();
+        }
+    }
+
+    protected void tickPlayer(EntityPlayer player)
+    {
+        if (player instanceof EntityPlayerMP)
+            processServer(player);
     }
 
 }
