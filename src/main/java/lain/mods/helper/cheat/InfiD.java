e74f02cd447707527735f22739b3113e41d342f2
玲音 <zlainsama@163.com>
2014-08-28 01:05:18 +0800

    backup

--- src/main/java/lain/mods/helper/cheat/InfiD.java
+++ src/main/java/lain/mods/helper/cheat/InfiD.java
@@ -1,6 +1,5 @@
 package lain.mods.helper.cheat;
 
-import java.util.Set;
 import lain.mods.helper.note.Note;
 import lain.mods.helper.note.NoteClient;
 import net.minecraft.client.entity.EntityClientPlayerMP;
@@ -9,70 +8,13 @@ import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.entity.living.LivingAttackEvent;
 import net.minecraftforge.event.entity.living.LivingHurtEvent;
-import com.google.common.collect.ImmutableSet;
-import com.google.common.collect.Sets;
 import cpw.mods.fml.common.FMLCommonHandler;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import cpw.mods.fml.common.gameevent.TickEvent;
 
 public class InfiD
 {
-
-    enum immuneDamages
-    {
-        drown,
-        wither,
-        suffocate,
-        oxygenSuffocation,
-        radiation;
-
-        static Set<String> c;
-        static
-        {
-            Set<String> tmp = Sets.newHashSet();
-            for (immuneDamages e : immuneDamages.values())
-                tmp.add(e.type);
-            c = ImmutableSet.copyOf(tmp);
-        }
-
-        final String type;
-
-        immuneDamages()
-        {
-            this.type = name();
-        }
-    }
-
-    enum immunePotions
-    {
-        moveSlowdown(2),
-        digSlowdown(4),
-        confusion(9),
-        blindness(15),
-        hunger(17),
-        weakness(18),
-        poison(19),
-        wither(20),
-        radiation(24);
-
-        static Set<Integer> c;
-        static
-        {
-            Set<Integer> tmp = Sets.newHashSet();
-            for (immunePotions e : immunePotions.values())
-                tmp.add(e.id);
-            c = ImmutableSet.copyOf(tmp);
-        }
-
-        final int id;
-
-        immunePotions(int id)
-        {
-            this.id = id;
-        }
-    }
 
     public static void load()
     {
@@ -92,19 +34,6 @@ public class InfiD
 
     private InfiD()
     {
-    }
-
-    @SubscribeEvent
-    public void handleEvent(LivingAttackEvent event)
-    {
-        if (event.entityLiving instanceof EntityPlayerMP)
-        {
-            if (Note.getNote((EntityPlayerMP) event.entityLiving).get("InfiD") != null)
-            {
-                if (immuneDamages.c.contains(event.source.getDamageType()))
-                    event.setCanceled(true);
-            }
-        }
     }
 
     @SubscribeEvent
@@ -138,10 +67,8 @@ public class InfiD
                     repairItem(item);
             }
 
-            for (int p : immunePotions.c)
-                player.removePotionEffectClient(p);
-
-            player.extinguish();
+            if (player.fallDistance > 1.0F)
+                player.fallDistance = 1.0F;
         }
     }
 
@@ -156,10 +83,8 @@ public class InfiD
                     repairItem(item);
             }
 
-            for (int p : immunePotions.c)
-                player.removePotionEffect(p);
-
-            player.extinguish();
+            if (player.fallDistance > 1.0F)
+                player.fallDistance = 1.0F;
         }
     }
 
