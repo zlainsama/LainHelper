00bbc98b9028150671f6247a511bba28a3c24b5f
玲音 <zlainsama@163.com>
2014-08-29 20:41:07 +0800

    backup

--- src/main/java/lain/mods/helper/cheat/InfiD.java
+++ src/main/java/lain/mods/helper/cheat/InfiD.java
@@ -1,20 +1,45 @@
 package lain.mods.helper.cheat;
 
+import ic2.api.item.ElectricItem;
+import ic2.api.item.IElectricItem;
 import lain.mods.helper.note.Note;
 import lain.mods.helper.note.NoteClient;
+import mekanism.api.energy.IEnergizedItem;
 import net.minecraft.client.entity.EntityClientPlayerMP;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.item.ItemStack;
-import net.minecraft.nbt.NBTTagCompound;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.entity.living.LivingHurtEvent;
+import universalelectricity.api.item.IEnergyItem;
+import appeng.api.implementations.items.IAEItemPowerStorage;
+import cofh.api.energy.IEnergyContainerItem;
 import cpw.mods.fml.common.FMLCommonHandler;
+import cpw.mods.fml.common.Loader;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import cpw.mods.fml.common.gameevent.TickEvent;
 
 public class InfiD
 {
+
+    enum ModCompat
+    {
+
+        IC2("IC2"),
+        COFH("CoFHCore"),
+        UE("UniversalElectricity"),
+        AE2("appliedenergistics2"),
+        Mekanism("Mekanism");
+
+        public final String modId;
+        public final boolean available;
+
+        ModCompat(String par1)
+        {
+            available = Loader.isModLoaded(modId = par1);
+        }
+
+    }
 
     public static void load()
     {
@@ -36,6 +61,69 @@ public class InfiD
     {
     }
 
+    void chargeItem(ItemStack item)
+    {
+        if (ModCompat.IC2.available)
+        {
+            if (ElectricItem.manager != null && item.getItem() instanceof IElectricItem)
+                ElectricItem.manager.charge(item, Double.MAX_VALUE, ((IElectricItem) item.getItem()).getTier(item), true, false);
+        }
+        if (ModCompat.COFH.available)
+        {
+            if (item.getItem() instanceof IEnergyContainerItem)
+            {
+                int n = Integer.MAX_VALUE;
+                while (n > 0)
+                {
+                    int a = ((IEnergyContainerItem) item.getItem()).receiveEnergy(item, n, false);
+                    if (a > 0)
+                        n -= a;
+                    else
+                        break;
+                }
+            }
+        }
+        if (ModCompat.UE.available)
+        {
+            if (item.getItem() instanceof IEnergyItem)
+            {
+                double n = Double.MAX_VALUE;
+                while (n > 0)
+                {
+                    double a = ((IEnergyItem) item.getItem()).recharge(item, n, true);
+                    if (a > 0)
+                        n -= a;
+                    else
+                        break;
+                }
+            }
+        }
+        if (ModCompat.AE2.available)
+        {
+            if (item.getItem() instanceof IAEItemPowerStorage)
+            {
+                double n = Double.MAX_VALUE;
+                while (n > 0)
+                {
+                    double a = n - ((IAEItemPowerStorage) item.getItem()).injectAEPower(item, n);
+                    if (a > 0)
+                        n -= a;
+                    else
+                        break;
+                }
+            }
+        }
+        if (ModCompat.UE.available)
+        {
+            if (item.getItem() instanceof IEnergizedItem)
+            {
+                IEnergizedItem iei = (IEnergizedItem) item.getItem();
+                if (iei.canReceive(item) && iei.getEnergy(item) < iei.getMaxEnergy(item))
+                    iei.setEnergy(item, iei.getMaxEnergy(item));
+            }
+        }
+    }
+
     @SubscribeEvent
     public void handleEvent(LivingHurtEvent event)
     {
@@ -44,7 +132,7 @@ public class InfiD
             if (Note.getNote((EntityPlayerMP) event.entityLiving).get("InfiD") != null)
             {
                 if (event.ammount > 0)
-                    event.ammount *= 0.5F;
+                    event.ammount *= 0.2F;
             }
         }
     }
@@ -60,12 +148,12 @@ public class InfiD
     {
         if (NoteClient.instance().get("InfiD") != null)
         {
-            for (int i = 0; i < 5; i++)
-            {
-                ItemStack item = player.getEquipmentInSlot(i);
-                if (item != null)
-                    repairItem(item);
-            }
+            for (int i = 0; i < player.inventory.armorInventory.length; i++)
+                if (player.inventory.armorInventory[i] != null)
+                    chargeItem(player.inventory.armorInventory[i]);
+            for (int i = 0; i < player.inventory.mainInventory.length; i++)
+                if (player.inventory.mainInventory[i] != null)
+                    chargeItem(player.inventory.mainInventory[i]);
 
             if (player.fallDistance > 1.0F)
                 player.fallDistance = 1.0F;
@@ -76,37 +164,15 @@ public class InfiD
     {
         if (Note.getNote((EntityPlayerMP) player).get("InfiD") != null)
         {
-            for (int i = 0; i < 5; i++)
-            {
-                ItemStack item = player.getEquipmentInSlot(i);
-                if (item != null)
-                    repairItem(item);
-            }
+            for (int i = 0; i < player.inventory.armorInventory.length; i++)
+                if (player.inventory.armorInventory[i] != null)
+                    chargeItem(player.inventory.armorInventory[i]);
+            for (int i = 0; i < player.inventory.mainInventory.length; i++)
+                if (player.inventory.mainInventory[i] != null)
+                    chargeItem(player.inventory.mainInventory[i]);
 
             if (player.fallDistance > 1.0F)
                 player.fallDistance = 1.0F;
-        }
-    }
-
-    void repairItem(ItemStack item)
-    {
-        if (item.isItemStackDamageable() && item.getItem().isRepairable())
-        {
-            if (item.getItemDamage() > 0)
-                item.setItemDamage(0);
-        }
-        if (item.hasTagCompound() && item.getTagCompound().hasKey("InfiTool"))
-        {
-            NBTTagCompound data = item.getTagCompound().getCompoundTag("InfiTool");
-            if (!data.hasKey("Energy"))
-            {
-                if (data.getBoolean("Broken"))
-                    data.setBoolean("Broken", false);
-                if (data.getInteger("Damage") > 0)
-                    data.setInteger("Damage", 0);
-                if (item.getItemDamage() > 0) // visual
-                    item.setItemDamage(0);
-            }
         }
     }
 
