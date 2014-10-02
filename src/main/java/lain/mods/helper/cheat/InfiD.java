a47f5e63bd48e81f7fc106dc7f210fd1c3b74d85
玲音 <zlainsama@163.com>
2014-10-02 22:35:09 +0800

    backup

--- src/main/java/lain/mods/helper/cheat/InfiD.java
+++ src/main/java/lain/mods/helper/cheat/InfiD.java
@@ -8,32 +8,26 @@ import lain.mods.helper.note.NoteOption;
 import net.minecraft.client.entity.EntityClientPlayerMP;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.entity.living.LivingFallEvent;
+import net.minecraftforge.event.entity.player.PlayerFlyableFallEvent;
 import com.google.common.collect.ImmutableSet;
 import cpw.mods.fml.common.FMLCommonHandler;
 import cpw.mods.fml.common.eventhandler.EventPriority;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import cpw.mods.fml.common.gameevent.TickEvent;
-import cpw.mods.fml.relauncher.Side;
-import cpw.mods.fml.relauncher.SideOnly;
 
 public class InfiD
 {
 
     private static final Set<UUID> _MYID = ImmutableSet.of(UUID.fromString("17d81212-fc40-4920-a19e-173752e9ed49"), UUID.fromString("1c83e5b7-40f3-3d29-854d-e922c24bd362"));
+    private static final boolean isClient = FMLCommonHandler.instance().getSide().isClient();
 
     public static void load()
     {
-        InfiD iD = FMLCommonHandler.instance().getSide().isClient() ? new InfiD()
-        {
-            @Override
-            void tickPlayer(EntityPlayer player)
-            {
-                if (player instanceof EntityClientPlayerMP)
-                    processClient((EntityClientPlayerMP) player);
-                super.tickPlayer(player);
-            }
-        } : new InfiD();
+        InfiD iD = new InfiD();
         FMLCommonHandler.instance().bus().register(iD);
+        MinecraftForge.EVENT_BUS.register(iD);
     }
 
     private InfiD()
@@ -56,33 +50,69 @@ public class InfiD
     }
 
     @SubscribeEvent(priority = EventPriority.HIGH)
+    public void handleEvent(LivingFallEvent event)
+    {
+        if (event.entityLiving instanceof EntityPlayer)
+        {
+            EntityPlayer player = (EntityPlayer) event.entityLiving;
+            if (player instanceof EntityPlayerMP)
+            {
+                if (checkPlayerAccess((EntityPlayerMP) player))
+                {
+                    event.distance = 0.0F;
+                }
+            }
+            else if (isClient && player instanceof EntityClientPlayerMP)
+            {
+                if (checkPlayerAccessClient())
+                {
+                    event.distance = 0.0F;
+                }
+            }
+        }
+    }
+
+    @SubscribeEvent(priority = EventPriority.HIGH)
+    public void handleEvent(PlayerFlyableFallEvent event)
+    {
+        EntityPlayer player = event.entityPlayer;
+        if (player instanceof EntityPlayerMP)
+        {
+            if (checkPlayerAccess((EntityPlayerMP) player))
+            {
+                event.distance = 0.0F;
+            }
+        }
+        else if (isClient && player instanceof EntityClientPlayerMP)
+        {
+            if (checkPlayerAccessClient())
+            {
+                event.distance = 0.0F;
+            }
+        }
+    }
+
+    @SubscribeEvent(priority = EventPriority.HIGH)
     public void handleEvent(TickEvent.PlayerTickEvent event)
     {
         if (event.phase == TickEvent.Phase.END)
-            tickPlayer(event.player);
-    }
-
-    @SideOnly(Side.CLIENT)
-    void processClient(EntityClientPlayerMP player)
-    {
-        if (checkPlayerAccessClient())
         {
-            player.removePotionEffectClient(17);
+            EntityPlayer player = event.player;
+            if (player instanceof EntityPlayerMP)
+            {
+                if (checkPlayerAccess((EntityPlayerMP) player))
+                {
+                    player.removePotionEffect(17);
+                }
+            }
+            else if (isClient && player instanceof EntityClientPlayerMP)
+            {
+                if (checkPlayerAccessClient())
+                {
+                    player.removePotionEffectClient(17);
+                }
+            }
         }
-    }
-
-    void processServer(EntityPlayerMP player)
-    {
-        if (checkPlayerAccess(player))
-        {
-            player.removePotionEffect(17);
-        }
-    }
-
-    void tickPlayer(EntityPlayer player)
-    {
-        if (player instanceof EntityPlayerMP)
-            processServer((EntityPlayerMP) player);
     }
 
 }
