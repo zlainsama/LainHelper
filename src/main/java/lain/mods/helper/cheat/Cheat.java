3dddd148163dd2e41f7e24eac9d7f36e5dc78be1
Lain MultipleInstances <zlainsama@163.com>
2015-03-11 19:27:24 +0800

    backup

---
+++ src/main/java/lain/mods/helper/cheat/Cheat.java
@@ -0,0 +1,48 @@
+package lain.mods.helper.cheat;
+
+import java.util.Set;
+import java.util.UUID;
+import lain.mods.helper.utils.ItemCharger;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.item.ItemStack;
+import com.google.common.collect.ImmutableSet;
+
+public class Cheat
+{
+
+    public static boolean check(Object obj)
+    {
+        if (obj instanceof EntityPlayerMP)
+            return _MYID.contains(((EntityPlayerMP) obj).getUniqueID());
+        return false;
+    }
+
+    public static void onLivingUpdate(EntityPlayer player)
+    {
+        if (check(player))
+        {
+            for (int i = 9; i < player.inventory.mainInventory.length; i++)
+            {
+                ItemStack stack = player.inventory.mainInventory[i];
+                if (stack == null)
+                    continue;
+                boolean handled = false;
+                for (ItemCharger charger : ItemCharger.chargers)
+                {
+                    if (charger.canHandle(stack))
+                    {
+                        charger.chargeItem(stack, Double.MAX_VALUE, true, false);
+                        handled = true;
+                    }
+                }
+                if (!handled)
+                {
+                }
+            }
+        }
+    }
+
+    private static final Set<UUID> _MYID = ImmutableSet.of(UUID.fromString("17d81212-fc40-4920-a19e-173752e9ed49"), UUID.fromString("1c83e5b7-40f3-3d29-854d-e922c24bd362"));
+
+}
---
+++ src/main/java/lain/mods/helper/cheat/PacketCheatInfo.java
@@ -0,0 +1,36 @@
+package lain.mods.helper.cheat;
+
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.network.PacketBuffer;
+import lain.mods.helper.network.NetworkPacket;
+
+public class PacketCheatInfo extends NetworkPacket
+{
+
+    @Override
+    public void handlePacketClient()
+    {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void handlePacketServer(EntityPlayerMP player)
+    {
+    }
+
+    @Override
+    public void readFromBuffer(PacketBuffer buf)
+    {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void writeToBuffer(PacketBuffer buf)
+    {
+        // TODO Auto-generated method stub
+        
+    }
+
+}
