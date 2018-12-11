03192d251be03b2c6b89abd7163fc0c28362d80e
Lain MultipleInstances <zlainsama@163.com>
2018-12-12 04:19:41 +0800

    backup

--- src/main/java/lain/mods/helper/LainHelper.java
+++ src/main/java/lain/mods/helper/LainHelper.java
@@ -6,7 +6,7 @@ import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid = LainHelper.MODID, useMetadata = true, acceptedMinecraftVersions = "[1.12, 1.13)", certificateFingerprint = "aaaf83332a11df02406e9f266b1b65c1306f0f76")
+@Mod(modid = LainHelper.MODID, useMetadata = true, acceptedMinecraftVersions = "[1.12, 1.13)", acceptableRemoteVersions = "*", certificateFingerprint = "aaaf83332a11df02406e9f266b1b65c1306f0f76")
 public class LainHelper
 {
 
