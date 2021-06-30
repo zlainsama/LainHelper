366fc6428088322ae4f4ca58bee22029f798e4bd
Lain MultipleInstances <zlainsama@163.com>
2021-06-30 21:25:49 +0800

    backup

--- src/main/java/lain/mods/helper/cheat/impl/MasterEffect.java
+++ src/main/java/lain/mods/helper/cheat/impl/MasterEffect.java
@@ -15,27 +15,27 @@ public class MasterEffect implements Cheat {
     private static final UUID ArmorModifierID = UUID.fromString("34662888-411d-48f3-8772-8f2f2dba8db0");
 
     private static final AttributeModifier MasterHealthBoostModifier = new AttributeModifier(HealthBoostModifierID, HealthBoostModifierID.toString(), 20.0D, AttributeModifier.Operation.ADDITION);
-    private static final AttributeModifier MasterArmorToughnessModifier = new AttributeModifier(ArmorToughnessModifierID, ArmorToughnessModifierID.toString(), 4.0D, AttributeModifier.Operation.ADDITION);
-    private static final AttributeModifier MasterArmorModifier = new AttributeModifier(ArmorModifierID, ArmorModifierID.toString(), 12.0D, AttributeModifier.Operation.ADDITION);
+    private static final AttributeModifier MasterArmorModifier = new AttributeModifier(ArmorModifierID, ArmorModifierID.toString(), 20.0D, AttributeModifier.Operation.ADDITION);
+    private static final AttributeModifier MasterArmorToughnessModifier = new AttributeModifier(ArmorToughnessModifierID, ArmorToughnessModifierID.toString(), 12.0D, AttributeModifier.Operation.ADDITION);
     private static final AttributeModifier NormalHealthBoostModifier = new AttributeModifier(HealthBoostModifierID, HealthBoostModifierID.toString(), 20.0D, AttributeModifier.Operation.ADDITION);
-    private static final AttributeModifier NormalArmorToughnessModifier = new AttributeModifier(ArmorToughnessModifierID, ArmorToughnessModifierID.toString(), 4.0D, AttributeModifier.Operation.ADDITION);
-    private static final AttributeModifier NormalArmorModifier = new AttributeModifier(ArmorModifierID, ArmorModifierID.toString(), 12.0D, AttributeModifier.Operation.ADDITION);
+    private static final AttributeModifier NormalArmorModifier = new AttributeModifier(ArmorModifierID, ArmorModifierID.toString(), 20.0D, AttributeModifier.Operation.ADDITION);
+    private static final AttributeModifier NormalArmorToughnessModifier = new AttributeModifier(ArmorToughnessModifierID, ArmorToughnessModifierID.toString(), 12.0D, AttributeModifier.Operation.ADDITION);
 
     @Override
     public void onTick(PlayerEntity player) {
         if (player.isAlive()) {
             if (Master.test(player)) {
                 PlayerEffects.applyModifier(player, Attributes.MAX_HEALTH, MasterHealthBoostModifier);
-                PlayerEffects.applyModifier(player, Attributes.ARMOR_TOUGHNESS, MasterArmorToughnessModifier);
                 PlayerEffects.applyModifier(player, Attributes.ARMOR, MasterArmorModifier);
+                PlayerEffects.applyModifier(player, Attributes.ARMOR_TOUGHNESS, MasterArmorToughnessModifier);
                 PlayerEffects.applyEffect(player, Effects.DAMAGE_BOOST, 1);
                 PlayerEffects.applyEffect(player, Effects.REGENERATION, 0);
                 PlayerEffects.applyEffect(player, Effects.MOVEMENT_SPEED, 0);
                 PlayerEffects.applyEffect(player, Effects.DAMAGE_RESISTANCE, 1);
             } else {
                 PlayerEffects.applyModifier(player, Attributes.MAX_HEALTH, NormalHealthBoostModifier);
-                PlayerEffects.applyModifier(player, Attributes.ARMOR_TOUGHNESS, NormalArmorToughnessModifier);
                 PlayerEffects.applyModifier(player, Attributes.ARMOR, NormalArmorModifier);
+                PlayerEffects.applyModifier(player, Attributes.ARMOR_TOUGHNESS, NormalArmorToughnessModifier);
                 PlayerEffects.applyEffect(player, Effects.DAMAGE_BOOST, 0);
                 PlayerEffects.applyEffect(player, Effects.REGENERATION, 0);
                 PlayerEffects.applyEffect(player, Effects.MOVEMENT_SPEED, 0);
